4f9e9460e5864e482a663b072b53c9e3b38ffef9
Ignacius Berendes <iberendes01@gmail.com>
2016-11-05 11:04:26 -0600

    Update Styluscounter.cpp

--- Software/Styluscounter.cpp
+++ Software/Styluscounter.cpp
@@ -5,58 +5,60 @@ using namespace std;
 
 int main()
 {
-  //init
-	VideoCapture cam0(1);
-	if(!cam0.isOpened()) return 1;
+	//init
+	VideoCapture cam0(1);					//Use the first non-default camera.
+	if(!cam0.isOpened()) return 1;				//exit in case of connection error.
 
 	while(1)
 	{
-    //frames
-		Mat frame0;   //original image
-		Mat frame1;		//HSV image
-		Mat red;      //red pen threshold
-		Mat yellow;	  //yellow pen threshold
-		Mat blue;   	//blue pen threshold
-		vector<vector<Point> > bcontours;
-		vector<vector<Point> > ycontours;
-		vector<vector<Point> > rcontours;
+		//frames
+		Mat frame0;					//original image
+		Mat frame1;					//HSV image
+		Mat red;					//red pen threshold
+		Mat yellow;					//yellow pen threshold
+		Mat blue;					//blue pen threshold
+		
+		//Contour vectors
+		vector<vector<Point> > bcontours;		//Blue contours
+		vector<vector<Point> > ycontours;		//Yellow contours
+		vector<vector<Point> > rcontours;		//Red contours
 		vector<Vec4i> heirarchy;
     
-    //mapping
-    cam0 >> frame0;
+		//mapping
+		cam0>>frame0;
 
-    //convert to HSV
+		//convert to HSV
 		cvtColor(frame0,frame1,CV_BGR2HSV,0);
 
-    //Threshold shades of red
-    inRange(frame1,Scalar(120,128,128),Scalar(179,255,255),red);
+		//Threshold shades of red
+		inRange(frame1,Scalar(120,128,128),Scalar(179,255,255),red);
     
-    //Threshold shades of blue
+		//Threshold shades of blue
 		inRange(frame1,Scalar(100,128,128),Scalar(130,255,255),blue);
-
-    //Threshold shades of yellow
+		
+		//Threshold shades of yellow
 		inRange(frame1,Scalar(0,128,128),Scalar(58,255,255),yellow);
 
-    //Blue contours
-    findContours(blue,bcontours,heirarchy,CV_RETR_LIST,CV_CHAIN_APPROX_SIMPLE);
+		//Blue contours
+		findContours(blue,bcontours,heirarchy,CV_RETR_LIST,CV_CHAIN_APPROX_SIMPLE);
 
-    //Yellow contours
-    findContours(yellow,ycontours,heirarchy,CV_RETR_LIST,CV_CHAIN_APPROX_SIMPLE);
+		//Yellow contours
+		findContours(yellow,ycontours,heirarchy,CV_RETR_LIST,CV_CHAIN_APPROX_SIMPLE);
 
-    //Red contours
-    findContours(red,rcontours,heirarchy,CV_RETR_LIST,CV_CHAIN_APPROX_SIMPLE);
+		//Red contours
+		findContours(red,rcontours,heirarchy,CV_RETR_LIST,CV_CHAIN_APPROX_SIMPLE);
     
-    //Display each filtered image
-    imshow("HSV",frame1);
-    imshow("Original",frame0);
+		//Display each filtered image
+		imshow("HSV",frame1);
+		imshow("Original",frame0);
 		imshow("reds",red);
 		imshow("blues",blue);
 		imshow("yellows",yellow);
-    
-    //Print a table with the number of pens in each color
+		
+		//Print a table with the number of pens in each color
 		cout<<"Blue pens:"<<bcontours.size()<<"	"<<"Yellow pens:"<<ycontours.size()<<"	"<<"Red pens:"<<rcontours.size()<<endl;
-    
-    //Delay to prevent data overrun.
+		
+		//Delay to prevent data overrun.
 		int c=cvWaitKey(1);
 	}
 	return 0;
