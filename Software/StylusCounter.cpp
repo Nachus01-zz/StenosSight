6bd9e332fcd4efd6b54133bb779cafdb938f5a1c
Ignacius Berendes <iberendes01@gmail.com>
2016-11-27 13:52:23 -0600

    Update StylusCounter.cpp

--- Software/StylusCounter.cpp
+++ Software/StylusCounter.cpp
@@ -9,49 +9,49 @@ using namespace std;
 int main()
 {
 	//init
-	VideoCapture cam1(1);						//retreive image from (non-default) camera
-	if(!cam1.isOpened()) return 1;				//error if camera fails to load
+	VideoCapture cam1(1);				//retreive image from (non-default) camera
+	if(!cam1.isOpened()) return 1;			//error if camera fails to load
 
-    //variables
-    int erosion_sizeR=3;		                //erosion element size (square)
-    int erosion_sizeY=2;
-    int erosion_sizeB=3;
-    int dilation_sizeR0=2;  	                //dilation element size (square)
-    int dilation_sizeR=6;
-    int dilation_sizeY=6;
-    int dilation_sizeY0=1;
-    int dilation_sizeB=3;
-    Mat frame0;                     	        //original image
-    Mat frame1;                             	//median blur
-    Mat frame2;                             	//HSV image
-    Mat frame3;                             	//eroded image
-    Mat erosion_elementR = getStructuringElement(MORPH_RECT,Size(2*erosion_sizeR+1,2*erosion_sizeR+1),Point(erosion_sizeR,erosion_sizeR));
-    Mat erosion_elementY = getStructuringElement(MORPH_RECT,Size(2*erosion_sizeY+1,2*erosion_sizeY+1),Point(erosion_sizeY,erosion_sizeY));
-    Mat erosion_elementB = getStructuringElement(MORPH_RECT,Size(2*erosion_sizeB+1,2*erosion_sizeB+1),Point(erosion_sizeB,erosion_sizeB));
-    Mat dilation_elementR0 = getStructuringElement(MORPH_RECT,Size(2*dilation_sizeR0+1,2*dilation_sizeR0+1),Point(dilation_sizeR0,dilation_sizeR0));
-    Mat dilation_elementR = getStructuringElement(MORPH_RECT,Size(2*dilation_sizeR+1,2*dilation_sizeR+1),Point(dilation_sizeR,dilation_sizeR));
-    Mat dilation_elementY = getStructuringElement(MORPH_RECT,Size(2*dilation_sizeY+1,2*dilation_sizeY+1),Point(dilation_sizeY,dilation_sizeY));
-    Mat dilation_elementY0 = getStructuringElement(MORPH_RECT,Size(2*dilation_sizeY0+1,2*dilation_sizeY0+1),Point(dilation_sizeY0,dilation_sizeY0));
-    Mat dilation_elementB = getStructuringElement(MORPH_RECT,Size(2*dilation_sizeB+1,2*dilation_sizeB+1),Point(dilation_sizeB,dilation_sizeB));
-    Mat red;                                	//red pen threshold
-    Mat yellow;                             	//yellow pen threshold
-    Mat blue;                               	//blue pen threshold
-    Mat eroder;                             	//eroded red
-    Mat erodey;                             	//eroded yellow
-    Mat erodeb;                             	//eroded blue
-    Mat dilater0;                           	//dilated red
-    Mat dilater;
-    Mat dilatey;                            	//dilated yellow
-    Mat dilatey0;
-    Mat dilateb;                            	//dilated blue
-    Mat cloner;
-    Mat cloney;
-    Mat cloneb;
-    vector<vector<Point> > bcontours;       	//blue contours
-    vector<vector<Point> > ycontours;       	//yellow contours
-    vector<vector<Point> > rcontours;       	//red contours
+	//variables
+    	int erosion_sizeR=3;		                //erosion element size (square)
+    	int erosion_sizeY=2;
+    	int erosion_sizeB=3;
+    	int dilation_sizeR0=2;  	                //dilation element size (square)
+    	int dilation_sizeR=6;
+    	int dilation_sizeY=6;
+    	int dilation_sizeY0=1;
+    	int dilation_sizeB=3;
+    	Mat frame0;                     	        //original image
+    	Mat frame1;                             	//median blur
+    	Mat frame2;                             	//HSV image
+    	Mat frame3;                             	//eroded image
+    	Mat erosion_elementR = getStructuringElement(MORPH_RECT,Size(2*erosion_sizeR+1,2*erosion_sizeR+1),Point(erosion_sizeR,erosion_sizeR));
+    	Mat erosion_elementY = getStructuringElement(MORPH_RECT,Size(2*erosion_sizeY+1,2*erosion_sizeY+1),Point(erosion_sizeY,erosion_sizeY));
+    	Mat erosion_elementB = getStructuringElement(MORPH_RECT,Size(2*erosion_sizeB+1,2*erosion_sizeB+1),Point(erosion_sizeB,erosion_sizeB));
+    	Mat dilation_elementR0 = getStructuringElement(MORPH_RECT,Size(2*dilation_sizeR0+1,2*dilation_sizeR0+1),Point(dilation_sizeR0,dilation_sizeR0));
+    	Mat dilation_elementR = getStructuringElement(MORPH_RECT,Size(2*dilation_sizeR+1,2*dilation_sizeR+1),Point(dilation_sizeR,dilation_sizeR));
+    	Mat dilation_elementY = getStructuringElement(MORPH_RECT,Size(2*dilation_sizeY+1,2*dilation_sizeY+1),Point(dilation_sizeY,dilation_sizeY));
+    	Mat dilation_elementY0 = getStructuringElement(MORPH_RECT,Size(2*dilation_sizeY0+1,2*dilation_sizeY0+1),Point(dilation_sizeY0,dilation_sizeY0));
+    	Mat dilation_elementB = getStructuringElement(MORPH_RECT,Size(2*dilation_sizeB+1,2*dilation_sizeB+1),Point(dilation_sizeB,dilation_sizeB));
+    	Mat red;                                	//red pen threshold
+    	Mat yellow;                             	//yellow pen threshold
+    	Mat blue;                               	//blue pen threshold
+    	Mat eroder;                             	//eroded red
+    	Mat erodey;                             	//eroded yellow
+    	Mat erodeb;                             	//eroded blue
+    	Mat dilater0;                           	//dilated red
+    	Mat dilater;
+    	Mat dilatey;                            	//dilated yellow
+    	Mat dilatey0;
+    	Mat dilateb;                            	//dilated blue
+    	Mat cloner;
+    	Mat cloney;
+    	Mat cloneb;
+    	vector<vector<Point> > bcontours;       	//blue contours
+    	vector<vector<Point> > ycontours;       	//yellow contours
+    	vector<vector<Point> > rcontours;       	//red contours
 	vector<Vec4i> heirarchy;
-
+	
 	while(1)
 	{
 	  	//mapping
@@ -59,7 +59,7 @@ int main()
 
 		//median blur
 		for (int i=1; i<KERNEL; i=i+2)		//move kernel two pixels at a time
-       	{
+	       	{
 			medianBlur (frame0,frame1,i);
 		}
 
@@ -77,7 +77,7 @@ int main()
 
 		//1st dilate
 		dilate(red,dilater1,dilation_elementR0);
-        dilate(yellow,dilatey0,dilation_elementY0);
+        	dilate(yellow,dilatey0,dilation_elementY0);
 
 		//Erode
 		erode(dilater0,eroder, erosion_elementR);
